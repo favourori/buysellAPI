fd2306ef755a89f098b144014a58928eba543f0a
Favour Ori <orifavour2000@gmail.com>
2020-04-05 22:26:57 +0200

    config cors

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 let express = require("express");
 let app = express();
 
+require("dotenv").config();
+const mongoose = require("mongoose");
+//cors
+let cors = require("cors");
 
+//allowing cors
 
-app.get("/", (req, res)=>{
-    res.status(200).send({
-        success: true,
-        "message" : "BuySell API"
-    })
-})
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    success: true,
+    message: "BuySell API",
+  });
+});
 
 let PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
