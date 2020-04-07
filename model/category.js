6c905f55c07b1b05e32ee202e3308755876582d8
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-04-07 10:42:52 +0100

    Categories can be retrieved

--- model/category.js
+++ model/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const categorySchema = new mongoose.Schema({
         name: {type: String, required: [true, 'name is required']},
         description: {type: String, required: [true, 'description is required']},
-        image: String
+        image: {type: String, required: [true, 'image is required']}
     },
     {
         timestamps: true
