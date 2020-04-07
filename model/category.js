83484437b22a80e48f72f1246068c623c842299f
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-04-07 08:31:05 +0100

    Wrote category model

--- model/category.js
+++ model/category.js
@@ -1,7 +1,7 @@
  const mongoose = require('mongoose')
 
 const categorySchema = new mongoose.Schema({
-   title: {type: String, required: [true, 'Title is required']},
+   name: {type: String, required: [true, 'Title is required']},
    description: {type: String, required: [true, 'Description is required']}
 },
     {
