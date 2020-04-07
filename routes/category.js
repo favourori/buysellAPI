3d3b983f6312d636cd63e404eada1211a0cfe630
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-04-07 10:42:40 +0100

    Merge upload and creation of category

--- routes/category.js
+++ routes/category.js
@@ -1,19 +1,34 @@
 const router = require('express').Router();
 const Category = require('../model/category');
 
-router.post('/create', async (req, res) => {
-    try {
-        const category = await Category.create({...req.body});
-        return res.status(200)
-            .json({
-                success: true,
-                data: category,
-                message: 'Category created successfully'
-            });
-    } catch (err) {
-        return res.status(400).json({success: false, message: err.toString()});
-    }
-});
+router.post('/create', (() => {
+    const multer = require('multer');
+    const upload = multer({dest: 'public/categories/images'});
+    return [
+        upload.single('image'),
+        async (req, res) => {
+            try {
+                const category = await Category.create({
+                    ...req.body,
+                    image: (req.hostname !== 'localhost' && req.host || '') + '/' + req.file.path
+                });
+                return res.status(200)
+                    .json({
+                        success: true,
+                        data: category,
+                        message: 'Category image uploaded successfully'
+                    });
+            } catch (err) {
+                return res.status(400)
+                    .json({
+                        success: false,
+                        message: 'image could not be uploaded successfully'
+                    });
+            }
+        }
+    ]
+
+})());
 
 router.post('/delete', async (req, res) => {
     try {
@@ -46,36 +61,6 @@ router.get('/all', async (req, res) => {
             });
     }
 });
-
-router.post('/upload', (() => {
-    const multer = require('multer');
-    const upload = multer({dest: 'public/categories/images'});
-    return [
-        upload.single('image'),
-        async (req, res) => {
-            try {
-                const category = await Category.findOneAndUpdate(
-                    {_id: req.body.id,},
-                    {image: (req.host !== 'localhost' && req.host || '') + '/' + req.file.path},
-                    {new: true}
-                );
-                return res.status(200)
-                    .json({
-                        success: true,
-                        data: category,
-                        message: 'Category image uploaded successfully'
-                    });
-            } catch (err) {
-                return res.status(400)
-                    .json({
-                        success: false,
-                        message: 'image could not be uploaded successfully'
-                    });
-            }
-        }
-    ]
-
-})());
 
 
 module.exports = router;
