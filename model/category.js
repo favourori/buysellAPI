8faa0955ad349ce66eb1bf14b6eb5ff3224a1c9a
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-04-07 09:04:29 +0100

    Writeen uploading of file to a directory

--- model/category.js
+++ model/category.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const categorySchema = new mongoose.Schema({
         name: {type: String, required: [true, 'Title is required']},
         description: {type: String, required: [true, 'Description is required']},
-        image: {type: String, required: [true, 'Image is required']}
+        image: String
     },
     {
         timestamps: true
--- routes/category.js
+++ routes/category.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Category = require('../model/category')
+const path = require('path')
 
 router.post('/create', async (req, res) => {
     try {
@@ -27,6 +28,32 @@ router.post('/delete', async (req, res) => {
         return res.status(400).json({success: false, message: err.toString()})
     }
 })
+
+router.post('/upload', (async () => {
+    const multer = require('multer')
+    const upload = multer({dest: path.join(__dirname, '/../public/categories/images')})
+    return [
+        upload.single('image'),
+        async (req, res) => {
+            try {
+                const category = await Category.findOneAndUpdate({_id: req.body.id,}, {image: req.file.path}, {new: true})
+                return res.status(200)
+                    .json({
+                        success: true,
+                        data: category,
+                        message: 'Category image uploaded successfully'
+                    })
+            } catch (err) {
+                return res.status(400)
+                    .json({
+                        success: false,
+                        message: 'image could not be uploaded successfully'
+                    })
+            }
+        }
+    ]
+
+})())
 
 
 module.exports = router
