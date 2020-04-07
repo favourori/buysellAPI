69ca5fa30c67c81c3fa5f680613288ad62eeeb86
Oladiran Segun Solomon <sheghunoladiran9@gmail.com>
2020-04-07 13:10:42 +0100

    files extention now add and files deleted when category is deleted

--- model/category.js
+++ model/category.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const categorySchema = new mongoose.Schema({
-        name: {type: String, required: [true, 'name is required']},
-        description: {type: String, required: [true, 'description is required']},
-        image: {type: String, required: [true, 'image is required']}
+const categorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "name is required"],
+        },
+        description: {
+            type: String,
+            required: [true, "description is required"],
+        },
+        image: {
+            type: String,
+            required: [true, "image is required"],
+        },
     },
     {
-        timestamps: true
+        timestamps: true,
     }
-)
+);
 
-const Category = mongoose.model('category', categorySchema)
+const Category = mongoose.model("category", categorySchema);
 
-module.exports = Category
+module.exports = Category;
--- routes/category.js
+++ routes/category.js
@@ -1,66 +1,95 @@
-const router = require('express').Router();
-const Category = require('../model/category');
+const router = require("express").Router();
+const Category = require("../model/category");
+const fs = require("fs");
+const path = require("path");
 
-router.post('/create', (() => {
-    const multer = require('multer');
-    const upload = multer({dest: 'public/categories/images'});
-    return [
-        upload.single('image'),
-        async (req, res) => {
-            try {
-                const category = await Category.create({
-                    ...req.body,
-                    image: (req.hostname !== 'localhost' && req.host || '') + '/' + req.file.path
-                });
-                return res.status(200)
-                    .json({
+router.post(
+    "/create",
+    (() => {
+        const multer = require("multer");
+        const upload = multer({
+            storage: multer.diskStorage({
+                destination: (req, file, cb) => {
+                    cb(null, "public/categories/images");
+                },
+                filename: (req, file, cb) => {
+                    const fileNameArray = file.originalname.split(".");
+                    console.log(file);
+                    const filename =
+                        Date.now() +
+                        "." +
+                        fileNameArray[fileNameArray.length - 1];
+                    console.log(filename);
+                    cb(null, filename);
+                },
+            }),
+        });
+        return [
+            upload.single("image"),
+            async (req, res) => {
+                try {
+                    const category = await Category.create({
+                        ...req.body,
+                        image:
+                            ((req.hostname !== "localhost" && req.host) || "") +
+                            "/" +
+                            req.file.path,
+                    });
+                    return res.status(200).json({
                         success: true,
                         data: category,
-                        message: 'Category image uploaded successfully'
+                        message: "Category image uploaded successfully",
                     });
-            } catch (err) {
-                return res.status(400)
-                    .json({
+                } catch (err) {
+                    return res.status(400).json({
                         success: false,
-                        message: 'image could not be uploaded successfully'
+                        message: "image could not be uploaded successfully",
                     });
-            }
-        }
-    ]
+                }
+            },
+        ];
+    })()
+);
 
-})());
-
-router.post('/delete', async (req, res) => {
+router.post("/delete", async (req, res) => {
     try {
-        await Category.deleteOne({_id: req.body.id});
-        return res.status(200)
-            .json({
-                success: true,
-                message: 'Category deleted successfully'
-            });
+        const category = await Category.findOne({ _id: req.body.id });
+        const imagePath = path.join(
+            __dirname,
+            "/../",
+            `${category.image.replace(req.hostname, "")}`
+        );
+        fs.unlink(imagePath, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        await category.remove();
+        return res.status(200).json({
+            success: true,
+            message: "Category deleted successfully",
+        });
     } catch (err) {
-        return res.status(400).json({success: false, message: err.toString()});
+        return res
+            .status(400)
+            .json({ success: false, message: err.toString() });
     }
 });
 
-
-router.get('/all', async (req, res) => {
+router.get("/all", async (req, res) => {
     try {
         const categories = await Category.find();
-        return res.status(200)
-            .json({
-                success: true,
-                data: categories,
-                message: 'Categories retrieved successful'
-            });
+        return res.status(200).json({
+            success: true,
+            data: categories,
+            message: "Categories retrieved successful",
+        });
     } catch (err) {
-        return res.status(400)
-            .json({
-                success: false,
-                message: 'Could no retrieve categories'
-            });
+        return res.status(400).json({
+            success: false,
+            message: "Could no retrieve categories",
+        });
     }
 });
-
 
 module.exports = router;
